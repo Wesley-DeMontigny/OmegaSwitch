8c91cae9b9a44250b11d9a8d5866df2010cff977
Wesley DeMontigny <wdemont@umd.edu>
2025-11-14 17:51:54 +0000

    Removing the Bayesian optimization sims - they are very out of date

--- publication_analyses/bayesian_optimization/M0/run_sims.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-../../../build/main -treeOut ./trees.trees -mcmcOut analysis.log -branchOut branches.log -simulateM0 -M0 -simulationOutput simulation.log -sequentialTuningSim -bayesOpt 40000 -bayesOptFreq 1000 -numSimulations 20 -burnInIter 10000 -numIter 10000 -tuneFreq 250 -sampleFreq 25

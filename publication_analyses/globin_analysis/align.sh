2e2dd2b758b3a2a3f177cabeda78d060951fced0
Wesley DeMontigny <wdemont@umd.edu>
2026-04-03 20:38:44 +0000

    Removing publication analyses from the software repo

--- publication_analyses/globin_analysis/align.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mafft --maxiterate 1000 --globalpair globin_aa_source.fasta > globin_aa_aligned.fasta
--- publication_analyses/simulations/rj/run_simulation_batches.1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-../../../build/main -treeOut ./trees.1.trees -mcmcOut analysis.1.log -tipsOut tips.1.log -simulateRJ -RJ -simulationOutput simulation.1.log -numSimulations 25 -burnInIter 5000 -numIter 30000 -tuneFreq 100 -sampleFreq 50 &
-sleep 1
-../../../build/main -treeOut ./trees.2.trees -mcmcOut analysis.2.log -tipsOut tips.2.log -simulateRJ -RJ -simulationOutput simulation.2.log -numSimulations 25 -burnInIter 5000 -numIter 30000 -tuneFreq 100 -sampleFreq 50 &
